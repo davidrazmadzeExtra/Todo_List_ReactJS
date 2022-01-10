9205762db840e93fa3be7ad796241706a307ef88
David Razmadze <razd45@gmail.com>
2022-01-10 17:26:05 -0500

    Hardcode elements of the app (input, header, list, etc.)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hi</h1>
+      {/* 1. Header  */}
+      <h1>My Todo List</h1>
+
+      {/* 2. Add new item (input) */}
+      <input 
+        type='text'
+        placeholder='Add an item...'
+      />
+
+      {/* Add (button) */}
+      <button>
+        Add
+      </button> 
+
+      {/* 3. List of todos (unordered list) */}
+      <ul>
+        <li>Take out trash <button>x </button> </li>
+        <li>Mow the lawn</li>
+        <li>Grocery shopping</li>
+      </ul>
+
     </div>
   );
 }
