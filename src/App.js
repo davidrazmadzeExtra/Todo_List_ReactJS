7bc5872d5ac974a9c921da03e5fe5c7e12a95dcb
David Razmadze <razd45@gmail.com>
2022-01-11 15:44:36 -0500

    Change to item.id (not key)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ function App() {
 
   /* Adds a new item to the list array*/
   function addItem() {
+
+    // ! Check for empty item
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
+
     const item = {
       id: Math.floor(Math.random() * 1000),
       value: newItem
@@ -52,7 +59,7 @@ function App() {
       <ul>
         {items.map(item => {
           return(
-            <li key={item.key}>{item.value}<button className='delete-button' onClick={() => deleteItem(item.id)}>❌</button></li>
+            <li key={item.id}>{item.value}<button className='delete-button' onClick={() => deleteItem(item.id)}>❌</button></li>
           )
         })}
       </ul>
