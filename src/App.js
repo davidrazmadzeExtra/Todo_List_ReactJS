1db1e2e55dd1f8bbe322c076ee163cc605454bae
David Razmadze <razd45@gmail.com>
2022-01-18 16:35:43 -0500

    Detect when user clicks on item text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   // State Hook - `useState`
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
@@ -11,7 +10,6 @@ function App() {
 
   /* Adds a new item to the list array*/
   function addItem() {
-
     // ! Check for empty item
     if (!newItem) {
       alert("Press enter an item.");
@@ -20,11 +18,11 @@ function App() {
 
     const item = {
       id: Math.floor(Math.random() * 1000),
-      value: newItem
-    }
+      value: newItem,
+    };
 
     // Add new item to items array
-    setItems(oldList => [...oldList, item]);
+    setItems((oldList) => [...oldList, item]);
 
     // Reset newItem back to original state
     setNewItem("");
@@ -32,8 +30,13 @@ function App() {
 
   /* Deletes an item based on the `item.id` key */
   function deleteItem(id) {
-    const newArray = items.filter(item => item.id !== id);
+    const newArray = items.filter((item) => item.id !== id);
     setItems(newArray);
+  }
+
+  /* Edit an item text after creating it. */
+  function editItem(id) {
+    console.log(id);
   }
 
   // Main part of app
@@ -43,27 +46,32 @@ function App() {
       <h1>My Todo List</h1>
 
       {/* 2. Add new item (input) */}
-      <input 
-        type='text'
-        placeholder='Add an item...'
+      <input
+        type="text"
+        placeholder="Add an item..."
         value={newItem}
-        onChange={e => setNewItem(e.target.value)}
+        onChange={(e) => setNewItem(e.target.value)}
       />
 
       {/* Add (button) */}
-      <button onClick={() => addItem()}>
-        Add
-      </button>
+      <button onClick={() => addItem()}>Add</button>
 
       {/* 3. List of todos (unordered list) */}
       <ul>
-        {items.map(item => {
-          return(
-            <li key={item.id}>{item.value}<button className='delete-button' onClick={() => deleteItem(item.id)}>❌</button></li>
-          )
+        {items.map((item) => {
+          return (
+            <li key={item.id} onClick={() => editItem(item.id)}>
+              {item.value}
+              <button
+                className="delete-button"
+                onClick={() => deleteItem(item.id)}
+              >
+                ❌
+              </button>
+            </li>
+          );
         })}
       </ul>
-
     </div>
   );
 }
